7c80745e290ffb27eda04b6cbbc3a056e7847d62
DroidZed <aymen.dragon5@gmail.com>
2021-08-27 19:30:15 +0100

    fixed join and leave msgs

--- Cogs/joinsorleaves.py
+++ Cogs/joinsorleaves.py
@@ -1,26 +1,29 @@
 from discord import Member
-from discord.ext.commands import Cog, bot
-
-# TODO: Make this work.
+from discord.ext.commands import Cog
 
 
 class JoinsOrLeavesCog(Cog, name="joins or leaves commads"):
 
     def __init__(self, bot):
         self.bot = bot
+        self.out_channel = 880824817555738655
 
     @Cog.listener()
     async def on_member_join(self, member: Member):
-        channel = member.guild.get_channel(696842023625424947)
+        channel = member.guild.get_channel(self.out_channel)
         if channel is not None:
-            await channel.send("User has joined the server.")
+            await channel.send(f"<@{member.id}> has joined the server.")
+        else:
+            print('channel is none or missing perms')
 
     @Cog.listener()
-    async def on_member_leave(self, member):
-        channel = member.guild.get_channel(696842023625424947)
+    async def on_member_remove(self, member: Member):
+        channel = member.guild.get_channel(self.out_channel)
         if channel is not None:
-            await channel.send("User has left the server.")
+            await channel.send(f"<@{member.id}> has left the server.")
+        else:
+            print('channel is none or missing perms')
 
 
-def setup(bot: bot):
+def setup(bot):
     bot.add_cog(JoinsOrLeavesCog(bot))
