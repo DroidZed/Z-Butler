f8238d4c0af4afbc6239c68a1752f6dca71b0de2
DroidZed <aymen.dragon5@gmail.com>
2021-09-07 21:16:12 +0100

    🐛 FIX: a lot of errors..lazy to type /(' ^')/

--- config/embed/ban.py
+++ config/embed/ban.py
@@ -10,7 +10,7 @@ ban_config = {
     "author": {
         "name": "The Z Butler",
         "icon_url": "https://cdn.discordapp.com/avatars/759844892443672586/bb7df4730c048faacd8db6dd99291cdb.jpg"},
-    "thumbnail": "https://64.media.tumblr.com/fbeaedb718f8f4c23d261b100bbf62cc/tumblr_onv6j3by9b1uql2i0o1_500.gif",
+    "thumbnail": {"url": "https://64.media.tumblr.com/fbeaedb718f8f4c23d261b100bbf62cc/tumblr_onv6j3by9b1uql2i0o1_500.gif"},
     "footer": {
         "text": "Next time think twice before making trouble in a server 😶",
         "icon_url": "https://emoji.gg/assets/emoji/3886_BAN.gif"
