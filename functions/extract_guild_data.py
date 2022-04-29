29f2339f64a875e707883fb31be761e6dfd23860
DroidZed <aymen.dragon5@gmail.com>
2022-04-30 00:01:05 +0100

    tried to fix the server command

--- functions/extract_guild_data.py
+++ functions/extract_guild_data.py
@@ -3,13 +3,23 @@ from discord.ext.commands import Context
 
 
 def extract_guild_data(ctx: Context, guild: Guild) -> tuple[list[Role], int, int]:
-
     roles: list[Role] = list(
         filter(
             lambda role: role != ctx.guild.default_role and not role.managed,
             guild.roles,
         )
     )
+
+    for r in roles:
+        if r.id in {
+            896349097391444029,
+            898875325923094528,
+            898874934615482378,
+            898874121222516736,
+            969706983777263677,
+            969639120513163345,
+        }:
+            del r
 
     online_users_count: int = len(
         list(
