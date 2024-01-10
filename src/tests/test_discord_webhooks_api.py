d5940a194496bad5cffa4c4dff165d3bf0d2a8e2
DroidZed <41507665+DroidZed@users.noreply.github.com>
2024-01-10 12:57:08 +0100

    changed to poetry

---
+++ src/tests/test_discord_webhooks_api.py
@@ -0,0 +1,18 @@
+import pytest
+
+from modules.networking import RequestError
+from modules.networking import HttpAsyncClient
+
+
+class TestDiscordWebHooksApi:
+    @pytest.mark.asyncio
+    async def test_sendingMessage(self):
+        url = "https://discord.com/api/webhooks/1169582796877090846/qjyeBbeEQGE8dWOtXNgXQ5dkhoYHRXoD7OjJonkNDsC1sa_M87Y4ia24HWH5DlLPpNKg"
+
+        result = await HttpAsyncClient().post(
+            url, json={"content": "test test"}
+        )
+
+        assert not isinstance(result, RequestError)
+
+        print(result)
